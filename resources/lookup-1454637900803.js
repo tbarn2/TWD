cd313e7398747c5ceedb42b003e098818abbe1ad
Troy Barnard <troy.w.barnard@gmail.com>
2016-02-04 18:07:59 -0800

    new thing

---
+++ resources/lookup-1454637900803.js
@@ -0,0 +1,39 @@
+(function(window, undefined) {
+  var dictionary = {
+    "7c8750f8-7f4c-462d-92ea-d67090748d98": "Shop",
+    "9a334326-6d99-4385-a8d1-420b809fc011": "Rick Grimes",
+    "cf6d666c-ddc4-4a85-a820-d242ccab6ce9": "Cast",
+    "4f5b287e-ecee-470e-bfcc-a16088ce5d14": "Games",
+    "76ec98cd-3a2a-4b24-96a6-2f7e9aa82f29": "Relationship (Rick & Glenn)",
+    "257cb659-1c4f-43cc-b201-5562c8672377": "Episode 1",
+    "7fb59c99-e9a2-4b55-b599-a339069e412e": "Glenn Rhee",
+    "d12245cc-1680-458d-89dd-4f0d7fb22724": "Home Page",
+    "1f1ddf36-7a8e-4450-9ca0-52c2238e3ea7": "Discussion",
+    "87db3cf7-6bd4-40c3-b29c-45680fb11462": "960 grid - 16 columns",
+    "e5f958a4-53ae-426e-8c05-2f7d8e00b762": "960 grid - 12 columns",
+    "f39803f7-df02-4169-93eb-7547fb8c961a": "Template 1"
+  };
+
+  var uriRE = /^(\/#)?(screens|templates|masters)\/(.*)(\.html)?/;
+  window.lookUpURL = function(fragment) {
+    var matches = uriRE.exec(fragment || "") || [],
+        folder = matches[2] || "",
+        canvas = matches[3] || "",
+        name, url;
+    if(dictionary.hasOwnProperty(canvas)) { /* search by name */
+      url = folder + "/" + canvas;
+    }
+    return url;
+  };
+
+  window.lookUpName = function(fragment) {
+    var matches = uriRE.exec(fragment || "") || [],
+        folder = matches[2] || "",
+        canvas = matches[3] || "",
+        name, canvasName;
+    if(dictionary.hasOwnProperty(canvas)) { /* search by name */
+      canvasName = dictionary[canvas];
+    }
+    return canvasName;
+  };
+})(window);
---
+++ resources/screens/7fb59c99-e9a2-4b55-b599-a339069e412e-1454637900803.js
@@ -0,0 +1,169 @@
+jQuery("#simulation")
+  .on("click", ".s-7fb59c99-e9a2-4b55-b599-a339069e412e .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Image_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/d12245cc-1680-458d-89dd-4f0d7fb22724"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Text_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/d12245cc-1680-458d-89dd-4f0d7fb22724"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Text_2")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/cf6d666c-ddc4-4a85-a820-d242ccab6ce9"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Text_3")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/1f1ddf36-7a8e-4450-9ca0-52c2238e3ea7"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Text_4")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/4f5b287e-ecee-470e-bfcc-a16088ce5d14"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Text_5")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/7c8750f8-7f4c-462d-92ea-d67090748d98"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Tree_node_5")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/76ec98cd-3a2a-4b24-96a6-2f7e9aa82f29"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
